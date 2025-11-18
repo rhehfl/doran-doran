4a72bb77274f66b78db587360aa0ab735543fe58
GuDoYoon <rhehfl418q@gmail.com>
2025-11-18 14:44:04 +0900

    refactor(auth): remove GithubStrategy and clean up providers in AuthModule

--- packages/back/src/auth/auth.module.ts
+++ packages/back/src/auth/auth.module.ts
@@ -9,7 +9,6 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CookieModule } from '@/common/cookie/cookie.module';
 import { AuthGuard } from '@/auth/auth.guard';
-import { GithubStrategy } from '@/auth/strategies/github.strategy';
 
 @Module({
   imports: [
@@ -29,13 +28,7 @@ import { GithubStrategy } from '@/auth/strategies/github.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [
-    AuthService,
-    GoogleStrategy,
-    JwtStrategy,
-    AuthGuard,
-    GithubStrategy,
-  ],
+  providers: [AuthService, GoogleStrategy, JwtStrategy, AuthGuard],
   exports: [AuthGuard, JwtModule, AuthService],
 })
 export class AuthModule {}
