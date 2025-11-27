e1625457057ddd4bca40486539327eab0818f5d6
GuDoYoon <rhehfl418q@gmail.com>
2025-11-27 20:28:52 +0900

    feat: 채팅방 리스트 애니메이션추가

--- packages/front/src/app/chat-rooms/_components/ChatRoomList.tsx
+++ packages/front/src/app/chat-rooms/_components/ChatRoomList.tsx
@@ -5,6 +5,8 @@ import { ChatRoomCard } from "@/app/chat-rooms/_components";
 import { chatRoomQueries } from "@/app/_queries";
 import { useSearchParams } from "next/navigation";
 import PublicChatRoomCard from "@/app/chat-rooms/_components/PublicChatRoomCard";
+import { transition } from "@ssgoi/react";
+import { fade } from "@ssgoi/react/transitions";
 
 export default function ChatRoomList() {
   const searchParams = useSearchParams();
@@ -20,12 +22,26 @@ export default function ChatRoomList() {
   if (currentTab === "public") {
     if (publicChatRoomList.length === 0) {
       return (
-        <p className="text-center text-gray-500">공개된 채팅방이 없습니다.</p>
+        <p
+          className="text-center text-gray-500"
+          ref={transition({
+            key: "public-chatroom-empty",
+            ...fade(),
+          })}
+        >
+          공개된 채팅방이 없습니다.
+        </p>
       );
     }
 
     return (
-      <ul className="flex flex-col gap-5">
+      <ul
+        className="flex flex-col gap-5"
+        ref={transition({
+          key: "public-chatroom-list",
+          ...fade(),
+        })}
+      >
         {publicChatRoomList.map((chat) => (
           <PublicChatRoomCard key={chat.id} {...chat} />
         ))}
@@ -34,12 +50,26 @@ export default function ChatRoomList() {
   }
   if (chatRoomList.length === 0) {
     return (
-      <p className="text-center text-gray-500">참여중인 채팅방이 없습니다.</p>
+      <p
+        className="text-center text-gray-500"
+        ref={transition({
+          key: "private-chatroom-empty",
+          ...fade(),
+        })}
+      >
+        참여중인 채팅방이 없습니다.
+      </p>
     );
   }
 
   return (
-    <ul className="flex flex-col gap-5">
+    <ul
+      className="flex flex-col gap-5"
+      ref={transition({
+        key: "private-chatroom-list",
+        ...fade(),
+      })}
+    >
       {chatRoomList.map((chat) => (
         <ChatRoomCard key={chat.id} {...chat} />
       ))}
