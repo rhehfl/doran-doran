2ab68b1529b2eb8fd4e7c90ac40eb3068f9ef0e6
GuDoYoon <rhehfl418q@gmail.com>
2025-12-09 14:32:31 +0900

    fix: 타입 가져오기 문법 수정

--- packages/back/src/data-source.ts
+++ packages/back/src/data-source.ts
@@ -1,7 +1,7 @@
 import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
-import { DataSource, DataSourceOptions } from 'typeorm';
-import { SeederOptions } from 'typeorm-extension';
+import { DataSource, type DataSourceOptions } from 'typeorm';
+import type { SeederOptions } from 'typeorm-extension';
 
 config({
   path: './configs/env/.dev.env',
